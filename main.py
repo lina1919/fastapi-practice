b93935c62664ce0e37110c546da97b2f5f9bf4de
lina1919 <lina1919@naver.com>
2023-11-12 21:58:59 +0700

    feat : 회원가입 기능 추가

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from domain.question import question_router
+from domain.user import user_router
 
 app = FastAPI()
 
 app.include_router(question_router.router)
+app.include_router(user_router.router)
